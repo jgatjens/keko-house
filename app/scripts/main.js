5d53eb23d66cfa95bb600072a54eb1293b567b0e
Jairo Gatjens <jgatjens@gmail.com>
2015-11-18 22:17:19 -0600

    initil commit

---
+++ app/scripts/main.js
@@ -0,0 +1,34 @@
+/*var lan = {
+  es: {
+    title: 'Keko House'
+    detail: 'AMPLIA CASA GUACIMA (JARDIN)',
+    price: '$1200',
+    description: 'Casa de 3 pisos, 4 cuartos, estacionamiento 3 carros, 2 baños completos, cocina grande, sala grande, comedor, walk-in closet, oficina, cuarto de televisión y estudio, balcón, tercer piso mirador, cuarto de pilas, corredor amplio, jardín con árboles frutales, tapia a vuelta redonda con portón de metal, acceso a todos los servicios, recolección de basura 2 veces por semana, calle sin salida.'
+    benefits: ['Parada de buses a San Jose y Alajuela muy cerca.','A 2 min de la salida de la Ruta 27.','A 10 min del aeropuerto Juan Santamaria.','A 1 min del parque Viva.','A 25 min del parque de La Sabana (San Jose centro).','A 1 hora de la playa.','A 5 min de Automercado.']
+    furnished: '$300',
+    address: '400 mts norte y 100 mts este de Ottos bar en calle Veraneras, Guácima Arriba, Alajuela.',
+    phone: '8825-8789 Keko 8350-2825'
+  },
+  en: {
+    title: 'Casa Keko',
+    detail: 'LARGE HOUSE GUACIMA (GARDEN)',
+    price: '$1200',
+    description: '3 floor house, 4 bedrooms, 3 parking spaces, 2 bathrooms, large kitchen, large living room, dining area, walk-in closet, office, TV room, balcony, third floor room, laundry room, wide corridor, garden with fruit trees, a round back wall with metal gate, access to all services, garbage removal 2 times a week, end street.'
+    benefits: ['Bus stop to San Jose and Alajuela nearby.','2 min from the exit of Route 27.','10 minutes from the Juan Santamaria Airport.','1 min from Viva park.','25 minutes from La Sabana (San Jose center).','1 hour from the beach.','5 min from Automercado.']
+    furnished: '$300',
+    address: '400N and 100E Ottos bar Veraneras Street, Guácima Arriba, Alajuela.',
+    phone: '8825-8789 Keko 8350-2825'
+  }
+};
+
+*/
+
+
+// function initMap() {
+//   // Create a map object and specify the DOM element for display.
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: -34.397, lng: 150.644},
+//     scrollwheel: false,
+//     zoom: 8
+//   });
+// }
